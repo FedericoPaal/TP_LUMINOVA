5d2ae0e50a2747bacbef36fb08ee4a82a594e628
Dario <dgimenez.developer@gmail.com>
2025-06-27 13:26:33 -0300

    Se agrega lógica de notificación

--- App_LUMINOVA/context_processors.py
+++ App_LUMINOVA/context_processors.py
@@ -19,11 +19,17 @@ def notificaciones_context(request):
         estado='BORRADOR'
     ).count()
     
-    total_notificaciones = ops_con_problemas_count + solicitudes_insumos_count + ocs_para_aprobar_count
+    ocs_en_transito_count = Orden.objects.filter(
+        tipo='compra',
+        estado='EN_TRANSITO'
+    ).count()
+
+    total_notificaciones = ops_con_problemas_count + solicitudes_insumos_count + ocs_para_aprobar_count + ocs_en_transito_count
 
     return {
         'ops_con_problemas_count': ops_con_problemas_count,
         'solicitudes_insumos_count': solicitudes_insumos_count,
         'ocs_para_aprobar_count': ocs_para_aprobar_count,
+        'ocs_en_transito_count': ocs_en_transito_count,
         'total_notificaciones': total_notificaciones,
     }
